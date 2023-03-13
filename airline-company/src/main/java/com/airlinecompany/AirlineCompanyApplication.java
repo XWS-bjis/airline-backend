7adcd21461898c603810d47dd17e35f25dd24ab5
Jelena Petric <petricjelena00@gmail.com>
2023-03-13 19:15:01 +0100

    initial commit

---
+++ airline-company/src/main/java/com/airlinecompany/AirlineCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.airlinecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineCompanyApplication.class, args);
+	}
+
+}
---
+++ airline-company/src/test/java/com/airlinecompany/AirlineCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlinecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
