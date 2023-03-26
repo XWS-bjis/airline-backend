880d9a5285c881569094b1b4b449074ee7129320
Jelena Petric <petricjelena00@gmail.com>
2023-03-26 21:09:21 +0200

    fix bug

--- airline-company/src/main/java/com/airlinecompany/controller/ReservationController.java
+++ airline-company/src/main/java/com/airlinecompany/controller/ReservationController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/reservation")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ReservationController {
 
     @Autowired
